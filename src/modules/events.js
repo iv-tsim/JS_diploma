537dc3afcda02ee2f7e8c426278bad350a811839
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-05-14 14:59:56 +0300

    dropdown menu is working

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,5 +1,10 @@
 export default function events() {
     document.body.addEventListener('click', event => {
-        console.log(event.target);
-    })
+        const {target} = event;
+        const clubSelectUl = document.querySelector('.clubs-list ul');
+        const toggleDisplay = element => {getComputedStyle(element).display === 'none' ? element.style.display = 'block' : element.style.display = 'none';};
+        if (target.closest('.clubs-list')) {
+            toggleDisplay(clubSelectUl);
+        }
+    });
 }
