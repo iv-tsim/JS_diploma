b511cad235832d2340b6db4a86f2bf09d4db5fad
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-05-14 16:11:21 +0300

    modal 1 and 2 are working

--- src/modules/events.js
+++ src/modules/events.js
@@ -1,10 +1,21 @@
 export default function events() {
     document.body.addEventListener('click', event => {
         const {target} = event;
+        console.log(target);
         const clubSelectUl = document.querySelector('.clubs-list ul');
-        const toggleDisplay = element => {getComputedStyle(element).display === 'none' ? element.style.display = 'block' : element.style.display = 'none';};
+        const forms = document.querySelectorAll('.popup');
+        const toggleForm = (id) => {
+            document.getElementById(id.slice(1)).classList.toggle('active');
+        }
         if (target.closest('.clubs-list')) {
-            toggleDisplay(clubSelectUl);
+            console.log(1);
+            clubSelectUl.classList.toggle('active');
+        }
+        if (target.dataset.popup === '#callback_form' || target.dataset.popup === '#free_visit_form') {
+            toggleForm(target.dataset.popup);
+        }
+        if (target.classList.contains('close_icon') || target.classList.contains('overlay')) {
+            forms.forEach(item => item.classList.remove('active'));
         }
     });
 }
